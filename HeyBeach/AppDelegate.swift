a2a0a26f4ee6b81c470b502f4440d5913959d17d
Pitbull305 <>
2019-02-02 10:29:04 +0100

    Created Xcode project

---
+++ HeyBeach/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  HeyBeach
+//
+
+import UIKit
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Properties
+    var window: UIWindow?
+    
+    // MARK: - UIApplicationDelegate
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
