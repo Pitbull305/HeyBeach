71b98e31a284d061ad212c953d65761887bf1294
Pitbull305 <>
2019-02-03 20:15:10 +0100

    Some refactoring

--- HeyBeach/UserDefaults/UserDefaultsManager.swift
+++ HeyBeach/UserDefaults/UserDefaultsManager.swift
@@ -9,18 +9,18 @@ class UserDefaultsManager {
     
     // MARK: - Properties
     private static let standardUserDefaults = UserDefaults.standard
-    private static let tokenKey = "user_token"
+    private static let userTokenKey = "user_token"
     
     // MARK: - Methods
     static func storeUserToken(_ token: String) {
-        standardUserDefaults.set(token, forKey: tokenKey)
+        standardUserDefaults.set(token, forKey: userTokenKey)
     }
     
     static func getUserToken() -> String? {
-        return standardUserDefaults.value(forKey: tokenKey) as? String
+        return standardUserDefaults.value(forKey: userTokenKey) as? String
     }
     
     static func deleteUserToken() {
-        standardUserDefaults.removeObject(forKey: tokenKey)
+        standardUserDefaults.removeObject(forKey: userTokenKey)
     }
 }
